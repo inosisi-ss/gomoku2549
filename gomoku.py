7fd454bc73c5d85133b5c548a5f8d479d626c325
inosisi-ss <42808261+inosisi-ss@users.noreply.github.com>
2020-03-10 10:40:59 +0900

    Update gomoku.py

--- gomoku.py
+++ gomoku.py
@@ -14,22 +14,6 @@ while True:
         print(act)
         board[act[0], act[1]] = 3
     if (board!=0).all(): break
-#coding: utf-8
-import numpy as np
-board = np.zeros((7, 7))
-print(board)
-
-while True:
-    act = np.random.randint(0, 7, 2)
-    if (board[act[0], act[1]])==0:
-        print(act)
-        board[act[0], act[1]] = 2
-    if (board!=0).all(): break
-    act = np.random.randint(0, 7, 2)
-    if (board[act[0], act[1]])==0:
-        print(act)
-        board[act[0], act[1]] = 3
-    if (board!=0).all(): break
 
 def column(piece):
     colu = piece[0:-4]+piece[1:-3]+piece[2:-2]+piece[3:-1]
